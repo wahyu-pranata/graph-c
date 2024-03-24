c909b9f693244afddc53950e3bfc06b25abd0a43
Wahyu Pranata <wahyupranata125@gmail.com>
2024-03-25 00:15:21 +0800

    Chore: add degree counter

--- main.c
+++ main.c
@@ -9,33 +9,6 @@ int getVertex()
   return vertex;
 }
 
-void connectGraph(int vertexA, int vertexB, int **graph)
-{
-  char userInput = '\0';
-  int isFirstAttempt = 0;
-
-  while (userInput != 'y' && userInput != 'n')
-  {
-    scanf("%*c");
-    if (isFirstAttempt == 1)
-    {
-      printf("Masukkan nilai yang benar [y/n]\n");
-    }
-    printf("Apakah vertex %d dan vertex %d terhubung?\n", vertexA, vertexB);
-    scanf("%c", &userInput);
-
-    if (userInput == 'y')
-    {
-      graph[vertexA][vertexB] = 1;
-    }
-    else if (userInput == 'n')
-    {
-      graph[vertexA][vertexB] = 0;
-    }
-    isFirstAttempt = 1;
-  };
-}
-
 int **createGraph(int vertex)
 {
   int **graph = (int **)malloc((vertex * vertex) * sizeof(int));
@@ -56,6 +29,46 @@ int **createGraph(int vertex)
   return graph;
 }
 
+void connectGraph(int vertexA, int vertexB, int **graph)
+{
+  char userInput = '\0';
+  int isFirstAttempt = 1;
+
+  while (userInput != 'y' && userInput != 'n')
+  {
+    scanf("%*c");
+    if (isFirstAttempt == 0)
+    {
+      printf("Masukkan nilai yang benar [y/n]\n");
+    }
+    printf("Apakah vertex %d dan vertex %d terhubung?\n", vertexA, vertexB);
+    scanf("%c", &userInput);
+
+    if (userInput == 'y')
+    {
+      graph[vertexA][vertexB] = 1;
+    }
+    else if (userInput == 'n')
+    {
+      graph[vertexA][vertexB] = 0;
+    }
+    isFirstAttempt = 0;
+  };
+}
+
+void getDegree(int **graph, int vertex, int vertexNo)
+{
+  int count = 0;
+  for (int i = 0; i < vertex; i++)
+  {
+    if (graph[vertexNo][i] == 1)
+    {
+      count++;
+    }
+  }
+  printf("Derajat vertek %d adalah: %d\n", vertexNo, count);
+}
+
 int main()
 {
   int vertex = getVertex();
@@ -70,7 +83,7 @@ int main()
   // Banyak vertex berarti sama dengan banyak baris dan kolom
   for (int i = 0; i < vertex; i++)
   {
-    for (int j = 0; j < vertex; j++)
+    for (int j = i + 1; j < vertex; j++)
     {
       if (i == j)
       {
@@ -79,6 +92,7 @@ int main()
       else
       {
         connectGraph(i, j, graph);
+        graph[j][i] = graph[i][j];
       }
     }
   }
@@ -91,6 +105,10 @@ int main()
     }
     printf("\n");
   }
+
+  getDegree(graph, vertex, 1);
+
+  free(graph);
 
   return 0;
 }
